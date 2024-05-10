634756d0fb5412f82b7b108434d304e2324629fd
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-05-10 17:29:59 -0300

    modelam Veículo e Estruturando Migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get '/', controller: 'home', action: 'index'
+  # get '/', controller: 'home', action: 'index'
+  root 'home#index'
 end
+
+# C - Create
+# R - Read
+# U - Update
+# D - Delete
+# 7 ações
